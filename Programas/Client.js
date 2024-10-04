7a341ad3b58bf3f00c411dfc4f6a1940a9b0843d
FefeProgrammer2001 <144487551+FefeProgrammer2001@users.noreply.github.com>
2024-10-04 12:43:55 -0300

    Update Client.js

--- Programas/Client.js
+++ Programas/Client.js
@@ -1,58 +1,65 @@
 const net = require('net');
 
 const porta = 8080;
-const ip = '192.168.0.100'; // Substitua pelo IP do computador que está rodando o servidor
 
-const client = new net.Socket();
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-client.connect(porta, ip, () => {
-  console.log('Conectado ao servidor');
+rl.question('Digite o endereço IP do servidor: ', (ip) => {
+  const client = new net.Socket();
 
-  const readline = require('readline');
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+  client.connect(porta, ip, () => {
+    console.log('Conectado ao servidor');
 
-  rl.question('Digite um comando (enviar, listar, deletar): ', (comando) => {
-    switch (comando) {
-      case 'enviar':
-        rl.question('Digite o nome do arquivo a enviar: ', (arquivo) => {
-          rl.question('O arquivo já existe? (sim/não): ', (existe) => {
-            if (existe.toLowerCase() === 'sim') {
-              rl.question('Digite o nome do arquivo existente: ', (nomeArquivoExistente) => {
-                client.write(`enviar ${arquivo} existente ${nomeArquivoExistente}`);
-              });
-            } else {
-              rl.question('Digite o conteúdo do arquivo: ', (conteudo) => {
-                client.write(`enviar ${arquivo} ${conteudo}`);
-              });
-            }
+    rl.question('Digite um comando (enviar, listar, deletar, sair): ', (comando) => {
+      switch (comando) {
+        case 'enviar':
+          rl.question('Digite o nome do arquivo a enviar: ', (arquivo) => {
+            rl.question('O arquivo já existe? (sim/não): ', (existe) => {
+              if (existe.toLowerCase() === 'sim') {
+                rl.question('Digite o nome do arquivo existente: ', (nomeArquivoExistente) => {
+                  client.write(`enviar ${arquivo} existente ${nomeArquivoExistente}`);
+                });
+              } else {
+                rl.question('Digite o conteúdo do arquivo: ', (conteudo) => {
+                  client.write(`enviar ${arquivo} ${conteudo}`);
+                });
+              }
+            });
           });
-        });
-        break;
+          break;
 
-      case 'listar':
-        client.write('listar');
-        break;
+        case 'listar':
+          client.write('listar');
+          break;
 
-      case 'deletar':
-        rl.question('Digite o nome do arquivo a deletar: ', (arquivoDeletar) => {
-          client.write(`deletar ${arquivoDeletar}`);
-        });
-        break;
+        case 'deletar':
+          rl.question('Digite o nome do arquivo a deletar: ', (arquivoDeletar) => {
+            client.write(`deletar ${arquivoDeletar}`);
+          });
+          break;
 
-      default:
-        console.log('Comando inválido');
-        break;
-    }
-  });
+        case 'sair':
+          client.end();
+          console.log('Conexão encerrada');
+          process.exit(0);
+          break;
 
-  client.on('data', (data) => {
-    console.log(data.toString());
-  });
+        default:
+          console.log('Comando inválido');
+          break;
+      }
+    });
 
-  client.on('end', () => {
-    console.log('Desconectado do servidor');
+    client.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    client.on('end', () => {
+      console.log('Desconectado do servidor');
+    });
   });
 });
